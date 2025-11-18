f99e648e24636a3fed60e5df0ac5a11c5e56f32d
Xuan-Minh <naux.pro@gmail.com>
2025-11-18 16:52:07 +0100

    feat: ajouter de nouveaux composants BlogArchives, GalleryGridMore et ProjetCartel

---
+++ components/GalleryGridMore.jsx
@@ -0,0 +1,9 @@
+"use client";
+import React from "react";
+import { useTranslations } from "next-intl";
+import Lightbox from "yet-another-react-lightbox";
+
+export default function GalleryGridMore() {
+  const t = useTranslations();
+  return <section className=""></section>;
+}
