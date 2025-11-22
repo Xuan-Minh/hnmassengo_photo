851471bbafc62634f633a6aad5f8e6edd06ac9e4
Xuan-Minh <naux.pro@gmail.com>
2025-11-22 15:30:48 +0100

    ProjetCartel

--- components/ProjetCartel.jsx
+++ components/ProjetCartel.jsx
@@ -2,26 +2,56 @@
 import React, { useEffect } from "react";
 import { useTranslations } from "next-intl";
 import Lightbox from "yet-another-react-lightbox";
-export default function ProjetCarter() {
-  <div id="shop" className=" flex h-screen bg-whiteCustom font-playfair">
-    {/* Panier */}
-    <aside className="w-[320px] border-r border-black/30 p-10 flex flex-col">
-      <h2 className="text-3xl mb-8">cart</h2>
-      <ul className="flex-1 space-y-2 mb-8">
-        <li className="flex justify-between">
-          <span>1 “name project”</span>
-          <span>20€</span>
-        </li>
-      </ul>
-      <div className="border-t border-black/30 pt-4 flex justify-between text-lg">
-        <span>total</span>
-        <span>100€</span>
-      </div>
-      <button className="mt-4 text-gray-300 text-right" disabled>
-        → checkout
-      </button>
-    </aside>
+import OverlayActionButton from "./OverlayActionButton";
+import { motion } from "framer-motion";
 
-    <main className="flex-1 grid grid-cols-2 gap-10 p-16 justify-items-center"></main>
-  </div>;
+function ProjectMarquee() {
+  return (
+    <div className="absolute inset-y-0 bottom-0 border-t border-whiteCustom/60 overflow-hidden pointer-events-none">
+      <motion.div
+        className="whitespace-nowrap text-whiteCustom/90 font-playfair text-[42px] md:text-[56px] lg:text-[64px] py-2 -tracking-normal"
+        animate={{ y: ["0%", "-50%"] }}
+        transition={{ duration: 30, ease: "linear", repeat: Infinity }}
+        style={{ willChange: "transform" }}
+      ></motion.div>
+    </div>
+  );
+}
+
+export default function ProjetCartel() {
+  return (
+    <section
+      id="works"
+      className="h-screen snap-start flex items-center bg-whiteCustom"
+      aria-label="Section 2"
+    >
+      {/* Project Cartel */}
+
+      <main className="w-[55vw] h-screen border-r border-blackCustom p-16 flex flex-col justify-center">
+        <section className="flex flex-col items-start justify-center h-full">
+          <h2 className="text-4xl font-playfair mb-8 ">Project Name</h2>
+          <div className="font-playfair text-lg w-3/4 leading-relaxed indent-4">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+            posuere tincidunt lacus sit amet porttitor. Aliquam pharetra ante
+            vel nibh accumsan, a bibendum lorem egestas. Sed ac accumsan metus,
+            vitae finibus urna. Phasellus vel rhoncus nisl. Vestibulum congue
+            lacinia mi volutpat bibendum. Proin vitae odio est. Vivamus tempus
+            pretium commodo.
+          </div>
+          <div className="font-playfair text-lg w-3/4 leading-relaxed indent-4 ">
+            Nulla facilisi. Nam dui metus, interdum vitae lobortis vel, viverra
+            consequat neque. Praesent sagittis aliquet posuere. Aenean suscipit,
+            mi quis viverra pulvinar, purus nulla placerat mi, quis mollis
+            lectus ipsum vitae velit. Sed vehicula est in lobortis tincidunt.
+          </div>
+        </section>
+      </main>
+
+      {/* Marquee */}
+
+      <aside className="flex flex-col">
+        <ProjectMarquee />
+      </aside>
+    </section>
+  );
 }
