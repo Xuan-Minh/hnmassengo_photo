fd9f21ceef68d0cf44db6c94544001e33bcd8e11
Xuan-Minh <naux.pro@gmail.com>
2025-12-20 13:26:32 +0100

    feat: ajouter le bundle analyzer pour optimiser l'analyse des paquets

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 const createNextIntlPlugin = require("next-intl/plugin");
-
 const withNextIntl = createNextIntlPlugin();
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Tu peux ajouter tes autres configs Next.js ici si nécessaire
   turbopack: {
-    // Force le root de Turbopack au dossier du projet
     root: __dirname,
   },
 };
 
-module.exports = withNextIntl(nextConfig);
+module.exports = withBundleAnalyzer(withNextIntl(nextConfig));
