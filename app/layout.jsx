9ea26ed3d9690311a72ab973f9efa950588b45c7
Xuan-Minh <naux.pro@gmail.com>
2025-12-22 04:39:15 +0100

    feat: ajouter le composant SnipcartPortal pour intégrer Snipcart dans le layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,6 +6,7 @@ import IntroOverlay from "../components/IntroOverlay";
 import RevealRoot from "../components/RevealRoot";
 import ErrorBoundary from "../components/ErrorBoundary";
 import Script from "next/script";
+import SnipcartPortal from "../components/SnipcartPortal";
 
 export const metadata = {
   metadataBase: new URL("https://hannoahmassengo.fr"),
@@ -114,19 +115,7 @@ export default function RootLayout({ children }) {
           <RevealRoot>{children}</RevealRoot>
         </ErrorBoundary>
 
-        <div
-          id="snipcart"
-          data-api-key="MzhiNjU4MDUtYTA2MC00YjA5LTkwYmMtOWIyY2FjOTAyZmZlNjM4OTk4OTA5MTYxNDMxODU0"
-          data-config-modal-style="side"
-          data-config-add-product-behavior="none"
-          hidden
-          suppressHydrationWarning
-        ></div>
-        <Script
-          src="https://cdn.snipcart.com/themes/v3.0.31/default/snipcart.js"
-          strategy="afterInteractive"
-          suppressHydrationWarning
-        />
+        <SnipcartPortal />
       </body>
     </html>
   );
---
+++ components/SnipcartPortal.jsx
@@ -0,0 +1,22 @@
+"use client";
+import Script from "next/script";
+
+export default function SnipcartPortal() {
+  return (
+    <>
+      <div
+        id="snipcart"
+        data-api-key="MzhiNjU4MDUtYTA2MC00YjA5LTkwYmMtOWIyY2FjOTAyZmZlNjM4OTk4OTA5MTYxNDMxODU0"
+        data-config-modal-style="side"
+        data-config-add-product-behavior="none"
+        hidden
+        suppressHydrationWarning
+      ></div>
+      <Script
+        src="https://cdn.snipcart.com/themes/v3.0.31/default/snipcart.js"
+        strategy="afterInteractive"
+        suppressHydrationWarning
+      />
+    </>
+  );
+}
