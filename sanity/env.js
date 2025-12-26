5fbcb8b6093dafeab97da0fb41822b0567f7e3a4
Xuan-Minh <naux.pro@gmail.com>
2025-12-26 19:03:06 +0100

    fix: uniformiser les guillemets dans les messages d'erreur pour les variables d'environnement

--- sanity/env.js
+++ sanity/env.js
@@ -1,9 +1,9 @@
 export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-12-07'
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2025-12-07";
 
 export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
 export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
 
 if (!projectId) {
-  throw new Error('Missing NEXT_PUBLIC_SANITY_PROJECT_ID environment variable');
+  throw new Error("Missing NEXT_PUBLIC_SANITY_PROJECT_ID environment variable");
 }
