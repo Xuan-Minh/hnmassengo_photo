186d4fe1fdd703c333dbf3dd1d0528c58589625b
Xuan-Minh <naux.pro@gmail.com>
2025-12-26 19:03:01 +0100

    refactor: simplifier la création du client Sanity en extrayant les variables d'environnement

--- sanity/env.js
+++ sanity/env.js
@@ -3,3 +3,7 @@ export const apiVersion =
 
 export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
 export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+
+if (!projectId) {
+  throw new Error('Missing NEXT_PUBLIC_SANITY_PROJECT_ID environment variable');
+}
