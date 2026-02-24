fd87d0578753d6e5a98ec0e99518c66fabb575a6
Xuan-Minh <naux.pro@gmail.com>
2026-02-25 00:04:26 +0100

    reaza

--- components/overlays/LoadingOverlay.jsx
+++ components/overlays/LoadingOverlay.jsx
@@ -222,8 +222,6 @@ export default function LoadingOverlay() {
 
     // Précharger toutes les images
     imageSources.forEach((src, index) => {
-      if (index > 2) return;
-
       const img = typeof window !== 'undefined' ? new window.Image() : null;
       if (!img) return;
 
