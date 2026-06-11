f5ebe373558862643aabaa6108b095f8ce3f4765
Xuan-Minh <naux.pro@gmail.com>
2026-06-12 00:20:12 +0200

    dynamic import

--- src/i18n/request.js
+++ src/i18n/request.js
@@ -1,14 +1,30 @@
-import { getRequestConfig } from "next-intl/server";
-import { routing } from "./routing";
+import { getRequestConfig } from 'next-intl/server';
+import { routing } from './routing';
 
 export default getRequestConfig(async ({ requestLocale }) => {
-  const requested = await requestLocale;
-  const locale = routing.locales.includes(requested)
-    ? requested
-    : routing.defaultLocale;
+  let locale = await requestLocale;
+
+  if (!locale || !routing.locales.includes(locale)) {
+    locale = routing.defaultLocale;
+  }
+
+  // Utilisation de chemins statiques pour un code-splitting optimal (evite l'erreur no-dynamic-import-path)
+  let messages;
+  switch (locale) {
+    case 'en':
+      messages = (await import('../../messages/en.json')).default;
+      break;
+    case 'de':
+      messages = (await import('../../messages/de.json')).default;
+      break;
+    case 'fr':
+    default:
+      messages = (await import('../../messages/fr.json')).default;
+      break;
+  }
 
   return {
     locale,
-    messages: (await import(`../../messages/${locale}.json`)).default,
+    messages,
   };
 });
