163f6b4db51b9778d242344349055b073c3dfb23
Xuan-Minh <naux.pro@gmail.com>
2026-06-30 14:35:23 +0200

    structure

--- sanity/structure.js
+++ sanity/structure.js
@@ -3,15 +3,12 @@ export const structure = S =>
   S.list()
     .title('Content')
     .items([
-      S.listItem()
-        .title("Page d'accueil")
-        .id('homePage')
-        .child(
-          S.editor()
-            .id('homePage')
-            .schemaType('homePage')
-            .documentId('homePage')
-        ),
+      S.listItem().title("Page d'accueil").id('homePage').child(
+        S.editor()
+          .id('homePage')
+          .schemaType('homePage') // Le type de votre document
+          .documentId('c9ea024d-7509-446d-9e7f-afda42528d38') // L'ID exact du document retrouvé !
+      ),
       ...S.documentTypeListItems().filter(
         listItem => listItem.getId() !== 'homePage'
       ),
