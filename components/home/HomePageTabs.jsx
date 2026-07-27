cb1c6b6b8facc89913b95bdf37e9f0302c3016fc
Xuan-Minh <naux.pro@gmail.com>
2026-07-27 15:41:15 +0200

    size2xl

--- components/home/HomePageTabs.jsx
+++ components/home/HomePageTabs.jsx
@@ -349,7 +349,7 @@ function WindowItem({
         );
         return (
           <div className="z-50 flex flex-nowrap justify-around gap-2 w-[90vw] md:w-[40vw] lg:w-[25vw] 2xl:w-[25vw]">
-            <div className="w-[25%] 2xl:w-[20%] h-auto inline-block">
+            <div className="w-[25%] h-auto inline-block">
               {win.photo ? (
                 <Image
                   src={buildSanityImageUrl(win.photo)}
